11c1b1d5c94d632cd146fb6de758d45c6a14b3f0
simonhn <simonhn@gmail.com>
2011-07-18 14:24:36 +1000

    adjusting expire values for better caching

--- pavapi.rb
+++ pavapi.rb
@@ -68,8 +68,14 @@ configure do
 end
 
 #Caching 1 minute - must adjust
-before do
-  cache_control :public, :must_revalidate, :max_age => 60 unless development?
+
+
+before '/demo/*' do
+  cache_control :public, :must_revalidate, :max_age => 3600 unless development?
+end
+
+before '*/chart/*' do
+  cache_control :public, :must_revalidate, :max_age => 3600 unless development?
 end
 
 # Error 404 Page Not Found
